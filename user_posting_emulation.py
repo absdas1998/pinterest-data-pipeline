3c6e1f41976867647fe72dfda3f72516f18678ae
Abhishek Das <abhishekdas@Abhisheks-MacBook-Pro.local>
2023-09-12 16:29:25 +0100

    commit message

---
+++ user_posting_emulation.py
@@ -0,0 +1,91 @@
+import requests
+from time import sleep
+import random
+from multiprocessing import Process
+import boto3
+import json
+import sqlalchemy
+from sqlalchemy import text
+
+invoke_url = "https://9024l19ao4.execute-api.us-east-1.amazonaws.com/test"
+
+random.seed(100)
+
+
+class AWSDBConnector:
+
+    def __init__(self):
+
+        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
+        self.USER = 'project_user'
+        self.PASSWORD = ':t%;yCY3Yjg'
+        self.DATABASE = 'pinterest_data'
+        self.PORT = 3306
+        
+    def create_db_connector(self):
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        return engine
+
+
+new_connector = AWSDBConnector()
+
+def send_data_to_kafka(topic, data):
+    try:
+        kafka_topic = f"12a3410ba3cf.{topic}"  
+        response = requests.post(
+            f"{invoke_url}/topics/{kafka_topic}",
+            headers={"Content-Type": "application/vnd.kafka.json.v2+json"},
+            json=data
+        )
+        response.raise_for_status()
+        print(f"Data sent to Kafka topic {kafka_topic}")
+    except Exception as e:
+        print(f"Failed to send data to Kafka topic {kafka_topic}: {str(e)}")
+
+def run_infinite_post_data_loop():
+    while True:
+        sleep(random.randrange(0, 2))
+        random_row = random.randint(0, 11000)
+        engine = new_connector.create_db_connector()
+
+        with engine.connect() as connection:
+
+            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_selected_row = connection.execute(pin_string)
+            
+            for row in pin_selected_row:
+                pin_result = dict(row._mapping)
+            
+            send_data_to_kafka("pin", pin_result)
+
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
+            
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
+                geo_result['timestamp'] = geo_result['timestamp'].isoformat()
+
+            
+            send_data_to_kafka("geo", geo_result)
+
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
+            
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
+                user_result['date_joined'] = user_result['date_joined'].isoformat()
+            
+            send_data_to_kafka("user", user_result)
+            
+            # print(pin_result)
+            # print(geo_result)
+            # print(user_result)
+
+
+if __name__ == "__main__":
+    run_infinite_post_data_loop()
+    print('Working')
+    
+    
+
+
